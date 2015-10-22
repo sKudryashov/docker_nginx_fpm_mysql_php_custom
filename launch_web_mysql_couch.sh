22369a96836f1fc330bab8a1a44531794045abef
Sergey Kudryashov <s.a.kudryashov@gmail.com>
2015-10-22 10:35:14 +0300

    SSH server has been added

--- launch_web_mysql_couch.sh
+++ launch_web_mysql_couch.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-WORKDIR=$PWD'/../'
+WORKDIR=$PWD
 
-NGINX_DIR=$WORKDIR'/nginx/'
-NGINX_LOG=$NGINX_DIR'/log/'
-HOST_CONFIG=$NGINX_DIR'/conf.d/'
+NGINX_DIR=$WORKDIR'/nginx'
+NGINX_LOG=$NGINX_DIR'/log'
+HOST_CONFIG=$NGINX_DIR'/conf.d'
 DATABASE_DIR=$WORKDIR'/data'
 
 DATABASE_NAME='dbtest'
@@ -11,7 +11,7 @@ DATABASE_USER='dbtest_user'
 DATABASE_PASSWORD='123qwe'
 
 HOST_CONFIG=$HOST_CONFIG
-CONTAINER_CONFIG='/etc/nginx/conf.d/'
+CONTAINER_CONFIG='/etc/nginx/conf.d'
 
 XDEBUG_PORT=9001
 
@@ -26,8 +26,11 @@ CONTAINER_NAME='sfera/nginx-php-fpm:0.4'
 HOST_PORT=80
 CONTAINER_PORT=80
 
-DATABASE_HOST_VOLUME=$WORKDIR'/db/'$DATABASE_NAME'/'
-DATABASE_CONTAINER_VOLUME='/var/www/mysql/'$DATABASE_NAME'/'
+CONTAINER_SSH_PORT=22
+CONTAINER_HOST_PORT=23
+
+DATABASE_HOST_VOLUME=$WORKDIR'/db/'$DATABASE_NAME
+DATABASE_CONTAINER_VOLUME='/var/www/mysql/'$DATABASE_NAME
 
 NAME_WEB_STACK_CONTAINER='web-stack'
 NAME_SQL_CONTAINER='percona-db'
@@ -35,6 +38,8 @@ NAME_NOSQL_CONTAINER='couch-db'
 
 USER_INPUT=$1
 NAME=$(basename $0)
+
+echo $DATABASE_DIR
 
 function create_dirs {
   if [ ! -d $NGINX_DIR ]; then
@@ -62,14 +67,14 @@ function start_containers {
   docker rm $NAME_NOSQL_CONTAINER 2> /dev/null
 
   #https://hub.docker.com/_/percona/
-  docker run --rm --name=$NAME_SQL_CONTAINER -e DBNAME=$DATABASE_NAME -e DBUSER=$DATABASE_USER -e DBPASS=$DATABASE_PASSWORD \
- nicescale/percona-mysql /opt/nicedocker/create_db.sh
+  #docker run --rm --name=$NAME_SQL_CONTAINER -e DBNAME=$DATABASE_NAME -e DBUSER=$DATABASE_USER -e DBPASS=$DATABASE_PASSWORD nicescale/percona-mysql /opt/nicedocker/create_db.sh
+  docker run --name=$NAME_SQL_CONTAINER  -e MYSQL_ROOT_PASSWORD=$DATABASE_PASSWORD -d percona:latest
+  #https://github.com/klaemo/docker-couchdb
+  docker run -d -p 5984:5984 -v /$DATABASE_DIR:/usr/local/var/lib/couchdb --name=$NAME_NOSQL_CONTAINER klaemo/couchdb
 
-  docker run -d -p 5984:5984 -v $DATABASE_DIR:/usr/local/var/lib/couchdb --name=$NAME_NOSQL_CONTAINER klaemo/couchdb
-
-  docker run -d -p $XDEBUG_PORT:$XDEBUG_PORT -p $HOST_PORT:$CONTAINER_PORT -v $HOST_CONFIG:$CONTAINER_CONFIG -v \
-  $HOST_VOLUME:$CONTAINER_VOLUME -v $HOST_WEB_LOG:$CONTAINER_WEB_LOG --name=$NAME_WEB_STACK_CONTAINER \
-    --link=$NAME_NOSQL_CONTAINER:dbnosql $CONTAINER_NAME
+  docker run -d -p $XDEBUG_PORT:$XDEBUG_PORT -p $HOST_PORT:$CONTAINER_PORT -p $CONTAINER_HOST_PORT:$CONTAINER_SSH_PORT  \
+   -v /$HOST_CONFIG:$CONTAINER_CONFIG -v /$HOST_VOLUME:$CONTAINER_VOLUME \
+   -v /$HOST_WEB_LOG:$CONTAINER_WEB_LOG --name=$NAME_WEB_STACK_CONTAINER --link=$NAME_NOSQL_CONTAINER:dbnosql $CONTAINER_NAME
 
   docker ps
 }
